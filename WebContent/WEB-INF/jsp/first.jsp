f955e5de32ca7f3e63ef7839a01171fcb3974d52
2239063686@qq.com <2239063686@qq.com>
2020-04-03 22:32:08 +0800

    SpringMVC搭建完成 测试搭建环境成功

---
+++ WebContent/WEB-INF/jsp/first.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+  msg:${msg}
+  <br>
+  <c:forEach items="${list}" var="string">
+  ${string}
+  <br>
+  </c:forEach>
+</body>
+</html>
---
+++ src/Controller/UserController.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * 在配置式的springMVC中处理器需要实现接口Controller
+ * 这就相当于最初版的对servlet的封装，一个类处理一个请求
+ * @author Administrator
+ *
+ */
+
+public class UserController implements Controller{
+    List<String> slist;
+   
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mView = new ModelAndView();
+		mView.addObject("msg", "第一个SpringMVC/采用配置");
+		slist = new ArrayList<String>();
+		slist.add("hello");
+		slist.add("word");
+		mView.addObject("list", slist);
+		mView.setViewName("first");
+		return mView;
+	}
+	
+}
