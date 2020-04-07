6e5bb7652668880b2c117d4264307f1e2528032d
2239063686@qq.com <2239063686@qq.com>
2020-04-08 00:18:48 +0800

    添加user类

---
+++ src/Entity/User.java
@@ -0,0 +1,48 @@
+package Entity;
+
+public class User {
+	
+	String name;
+	
+	Integer age;
+	
+	public User(){}
+	
+	public User(String name, Integer age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+
+}
